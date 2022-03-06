42e019f6edf45bf84956742ae396dd364f8c5a93
jzfrank <frank.jin.zhang@outlook.com>
2022-03-06 22:16:03 +0100

    changed port setting

--- app.js
+++ app.js
@@ -47,6 +47,12 @@ app.post("/", function(req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// listen
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
